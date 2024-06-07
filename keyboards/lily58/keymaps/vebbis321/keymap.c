4bb1b3b9c1ddef03d006d942800122642608f247
vebbis321 <veb.dan@hotmail.com>
2024-06-07 14:53:18 +0200

    fixed error and compiled first keymap

--- keyboards/lily58/keymaps/vebbis321/keymap.c
+++ keyboards/lily58/keymaps/vebbis321/keymap.c
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
 CTL_T(KC_ESC),  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
       KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_BSLS,
-                            KC_RSFT, KC_LALT, KC_LGUI, KC_ENT, KC_SPC, MO(LOWER), MO(RAISE), KC_RALT
+                            KC_RSFT, KC_LALT, KC_LGUI, KC_ENT, KC_SPC, MO(_LOWER), MO(_RAISE), KC_RALT
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -46,11 +46,11 @@ CTL_T(KC_ESC),  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_
  *                   `----------------------------'           '------''--------------------'
  */
 [_LOWER] = LAYOUT(
-      KC_TILD,   KC_F1,   KC_F2,   KC_F3,   KC_F4, KC_F5,                   KC_F6, KC_F7, KC_F8,KC_F9, F10, KC_GRV,
+      KC_TILD,   KC_F1,   KC_F2,   KC_F3,   KC_F4, KC_F5,                   KC_F6, KC_F7, KC_F8,KC_F9, KC_F10, KC_GRV,
        KC_TAB,    KC_EXLM, KC_AT, KC_HASH, KC_DLR,KC_PERC,                   KC_CIRC,   KC_AMPR,   KC_ASTR,   KC_LPRN,  KC_RPRN,  KC_BSPC,
 CTL_T(KC_ESC),   KC_1,    KC_2,   KC_3,    KC_4,  KC_5,                     KC_UNDS, KC_PLUS, KC_LABK, KC_RABK, KC_QUES, KC_PIPE,
       KC_LSFT,   KC_6,    KC_7,   KC_8,    KC_9,    KC_0, _______, _______, KC_MINS, KC_EQL, KC_COMM, KC_DOT, KC_SLSH, KC_BSLS,
-                            KC_RSFT, KC_LALT, KC_LGUI, KC_ENT, KC_SPC, MO(LOWER), MO(RAISE), KC_RALT
+                            KC_RSFT, KC_LALT, KC_LGUI, KC_ENT, KC_SPC, MO(_LOWER), MO(_RAISE), KC_RALT
 ),
 /* RAISE
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -72,7 +72,7 @@ CTL_T(KC_ESC),   KC_1,    KC_2,   KC_3,    KC_4,  KC_5,                     KC_U
       KC_TAB,  KC_HOME, KC_END, KC_PGUP, KC_PGDN, KC_PSCR,                      KC_BRIU, KC_BRID,  KC_MUTE, KC_VOLD, KC_VOLU,  XXXXXXX,
 CTL_T(KC_ESC),  KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,                       XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX,
       KC_LSFT,   KC_F6,   KC_F7,   KC_F8,  KC_F9,  KC_F10,   _______, _______,  KC_F11, KC_F12, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX,
-                            KC_RSFT, KC_LALT, KC_LGUI, KC_ENT, KC_SPC, MO(LOWER), MO(RAISE), KC_RALT
+                            KC_RSFT, KC_LALT, KC_LGUI, KC_ENT, KC_SPC, MO(_LOWER), MO(_RAISE), KC_RALT
 ),
 };
 
